baf912cd825cd817682fd7f6b2638815ed73dec1
alaskanloops <alaskanloops@gmail.com>
2016-08-12 13:19:15 -0800

    Initial commit. Add new files. First view, controller, module done.

---
+++ app/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jmeyer on 8/10/2016.
+ * Module
+ */
+//function becomes self-executing
+//immediately invoked function expression
+//function expression within paranthesis
+(function () {
+    "use strict"; // in strict mode, javascript will catch some common javascript errors and throw exception
+      var app = angular.module("productManagement", []);
+}());
