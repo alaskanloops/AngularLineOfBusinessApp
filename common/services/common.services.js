d8c46f85248d47ae991338b07fb802d579be5117
alaskanloops <alaskanloops@gmail.com>
2016-08-15 10:26:56 -0800

    Delete .idea, add .gitignore, changes to files

---
+++ common/services/common.services.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jmeyer on 8/12/2016.
+ */
+(function () {
+    "use strict";
+    angular.module("common.services", ["ngResource"])
+}());
---
+++ common/services/productResource.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jmeyer on 8/12/2016.
+ */
+(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("productResource",
+                ["$resource",
+                productResource]);
+
+    function productResource($resource) {
+        return $resource("/api/products/:productId")
+    }
+
+}());
