f802abe8362054fc0c73fb8bac227317acbcfa87
alaskanloops <alaskanloops@gmail.com>
2016-08-19 15:12:25 -0800

    add validation and messages

--- app/products/productEditCtrl.js
+++ app/products/productEditCtrl.js
@@ -8,9 +8,10 @@
         .module("productManagement")
         .controller("ProductEditCtrl",
         ["product",
+            "$state",
         ProductEditCtrl]);
 
-    function ProductEditCtrl(product) {
+    function ProductEditCtrl(product, $state) {
         var vm = this;
 
         vm.product = product;
@@ -27,5 +28,33 @@
 
             vm.opened = !vm.opened;
         };
+
+        vm.submit = function (isValid) {
+            if (isValid) {
+                vm.product.$save(function (data) {
+                    toastr.success("Save Succesful");
+                });
+            } else {
+                alert("Please correct the validation errors first.");
+            }
+        };
+
+        vm.cancel = function () {
+            $state.go('productList');
+        };
+
+        vm.addTags = function (tags) {
+            if (tags) {
+                var array = tags.split(',');
+                vm.product.tags = vm.product.tags ? vm.product.tags.concat(array) : array;
+                vm.newTags = "";
+            } else {
+                alert("Please enter one or more tags separated by commas");
+            }
+        };
+
+        vm.removeTag = function (idx) {
+            vm.product.tags.splice(idx, 1);
+        }
     }
 }());
